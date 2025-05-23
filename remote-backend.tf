b878e56c6bc2ae72135f4d26ac4db587dee38959
Raghava1201 <145425948+Raghava1201@users.noreply.github.com>
2025-05-23 18:46:30 +0530

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "usecases-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
     use_lockfile = true
     encrypt = true
   }
